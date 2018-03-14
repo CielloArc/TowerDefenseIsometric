9fad1d7dca3ed0ae135d3266991d04eda66f8694
DESKTOP-THEBL69\Marcelo <max.ciello@hotmail.com>
2018-03-14 17:10:58 -0300

    Primeiros prefabs e scripts

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour
+{
+    protected Transform _target;
+
+    [Header("Projectile Stats")]
+    [SerializeField]
+    [Range(1f, 100f)]
+    protected float speed = 70f;
+
+    [SerializeField]
+    [Range(1f, 100f)]
+    protected int damage = 50;
+
+    [Header("Projectile Components")]
+    protected GameObject impactParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Seek(Transform target)
+    {
+        _target = target;
+    }
+}
