043a15bba801cb8e71dcf2ad168f63fd87fc0dbb
DESKTOP-THEBL69\Marcelo <max.ciello@hotmail.com>
2018-03-20 15:43:16 -0300

    20/MAR/2018 - Finished the prototype.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private Stats enemyStats;
+    [SerializeField]
+    private GameObject deathParticle;
+    
+    private int currentHealth;
+
+    private EnemyMovement movement;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Start ()
+    {
+        currentHealth = enemyStats.startingHealth;
+        movement = GetComponent<EnemyMovement>();
+        movement.SetBaseSpeed(enemyStats.baseSpeed);
+	}	
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Slow(float ammount)
+    {
+        movement.ChangeMoveSpeed(ammount);
+    }
+
+    public float GetBaseSpeed()
+    {
+        return enemyStats.baseSpeed;
+    }
+
+    void Die()
+    {
+        if (deathParticle != null)
+        {
+            GameObject newParticle = (GameObject)Instantiate(deathParticle, transform.position, transform.rotation);
+            Destroy(newParticle, 5f);
+        }
+
+        Spawner.Instance.OnEnemyDeath();
+        Player.Instance.AddCurrency(enemyStats.pointsValue);
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
 }
